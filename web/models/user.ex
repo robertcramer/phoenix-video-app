12fcf36169b5b1361bfb65a033c25e8eb52e178c
Robert Cramer <robertbcramer@icloud.com>
2016-09-05 19:00:19 -0400

    created annotations and persisted annotations

--- web/models/user.ex
+++ web/models/user.ex
@@ -7,6 +7,7 @@ defmodule Rumbl.User do
     field :password, :string, virtual: true
     field :password_hash, :string
     has_many :videos, Rumbl.Video
+    has_many :annotations, Rumbl.Annotation 
 
     timestamps
   end
@@ -15,6 +16,7 @@ defmodule Rumbl.User do
     model
     |> cast(params, ~w(name username), [])
     |> validate_length(:username, min: 1, max: 20)
+    |> unique_constraint(:username)
   end
 
   def registration_changeset(model, params) do
