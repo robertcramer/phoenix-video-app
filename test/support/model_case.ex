8a243bc66f6b6b789f6395dc229ea8a1dcb7777b
Robert Cramer <robertbcramer@icloud.com>
2016-09-05 13:40:28 -0400

    wrote model tests

--- test/support/model_case.ex
+++ test/support/model_case.ex
@@ -21,18 +21,21 @@ defmodule Rumbl.ModelCase do
       import Ecto
       import Ecto.Changeset
       import Ecto.Query
+      import Rumbl.TestHelpers
       import Rumbl.ModelCase
     end
   end
 
   setup tags do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Rumbl.Repo)
-
     unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(Rumbl.Repo, {:shared, self()})
+      Ecto.Adapters.SQL.restart_test_transaction(Rumbl.Repo, [])
     end
 
     :ok
+  end
+
+  def errors_on(model, data) do
+    model.__struct__.changeset(model, data).errors
   end
 
   @doc """
